7131bb67a3aba0e603929b23bd378ceb24b65bc4
harunsari53 <harunsari2303@gmail.com>
2023-05-30 16:08:32 +0300

    new commit

--- src/pages/DropdownAnimation/style.ts
+++ src/pages/DropdownAnimation/style.ts
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     shadowOpacity: .4,
     shadowColor: colors.black,
     position: 'absolute',
-      bottom: 20,
+      bottom: 22,
   },
   box: {
     width: 60,
